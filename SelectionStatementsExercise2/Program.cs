bc4790c2a16b1e14b4aa9f6cfe747210f3afbf32
Robert Anderson <rganderson0798@gmail.com>
2024-10-24 18:24:32 -0700

    favorite subjects

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            FavSubject();
+        }
+
+        public static void FavSubject()
+        {
+            Console.WriteLine("What is your favorite subject?");
+            string subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("I am math");
+                    break;
+                case "science":
+                    Console.WriteLine("Science rules!");
+                    break;
+                case "p.e.":
+                case "pe": 
+                case "physical education":
+                    Console.WriteLine("don't skip leg day");
+                    break;
+                case "history":
+                    Console.WriteLine("History is my favorite too");
+                    break;
+                case "geography":
+                    Console.WriteLine("I love to study the world");
+                    break;
+                default:
+                    Console.WriteLine("I like that too");
+                    break;
+            }
         }
     }
 }
